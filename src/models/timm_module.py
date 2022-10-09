5e130a8800649ad2062a765070dc8d4014f6362b
Sparsh Kedia <sparsh.kedia@gmail.com>
2022-10-09 07:27:57 +0000

    added jit trace export functions

--- src/models/timm_module.py
+++ src/models/timm_module.py
@@ -4,6 +4,8 @@ import torch
 from pytorch_lightning import LightningModule
 from torchmetrics import MaxMetric, MeanMetric
 from torchmetrics.classification.accuracy import Accuracy
+import torch.nn.functional as F
+import torchvision.transforms as T
 
 class TIMMLitModule(LightningModule):
 
@@ -35,9 +37,22 @@ class TIMMLitModule(LightningModule):
 
         # for tracking best so far validation accuracy
         self.val_acc_best = MaxMetric()
+        self.predict_transform = T.Normalize((0.1307,), (0.3081,))
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
+
+    @torch.jit.export
+    def forward_jit(self, x: torch.Tensor):
+        with torch.no_grad():
+            # transform the inputs
+            x = self.predict_transform(x)
+
+            # forward pass
+            logits = self(x)
+            preds = F.softmax(logits, dim=-1)
+
+        return preds
 
     def on_train_start(self):
         # by default lightning executes validation step sanity checks before training starts,
